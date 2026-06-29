819514c4e080bcf75990d6975876c60e696132a1
Charly Gomez <charly.gomez1310@gmail.com>
2026-06-29 15:09:23 +0200

    typo

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 //
 // These Sentry emissions are what we're verifying: under a Turbopack build,
 // do they reach Sentry in production? Compare against a `build:webpack` control.
-export async function middelware(request: NextRequest): Promise<NextResponse> {
+export async function middleware(request: NextRequest): Promise<NextResponse> {
   console.log('[middleware] hasClient =', !!Sentry.getClient());
   Sentry.logger.info('middleware hit', {
     path: request.nextUrl.pathname,
